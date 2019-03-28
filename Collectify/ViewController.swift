0a9d6ad77afe59c17b7b6e2077e6fada71a33b63
noah <noahwoodward2244@gmail.com>
2019-03-28 09:40:43 -0700

    First commit

---
+++ Collectify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collectify
+//
+//  Created by Noah Woodward on 3/28/19.
+//  Copyright © 2019 Noah Woodward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
