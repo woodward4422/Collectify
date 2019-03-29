b68a947c85fb7259efe7cde410c9b0f0ed34fe73
noah <noahwoodward2244@gmail.com>
2019-03-29 13:53:39 -0700

    Finished laying out the Product collectionview cell

---
+++ Collectify/View/Cells/CollectHeaderCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectHeaderCollectionViewCell.swift
+//  Collectify
+//
+//  Created by Noah Woodward on 3/29/19.
+//  Copyright © 2019 Noah Woodward. All rights reserved.
+//
+
+import UIKit
+
+class CollectHeaderCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Collectify/View/Cells/ProductCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCollectionViewCell.swift
+//  Collectify
+//
+//  Created by Noah Woodward on 3/29/19.
+//  Copyright © 2019 Noah Woodward. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
